468688e281f532fd31fbb620c775ac386c30d8c4
Sergiy <barsulsergij@gmail.com>
2023-11-07 08:07:19 -0800

    Gulp - watch method - lesson started

--- app/index.js
+++
@@ -1,5 +0,0 @@
-console.log('hello');
-const darkModePreference = null;
-const getUserDarkModePreference = (darkModePreference) => {
-  return darkModePreference ?? true;
-}
--- output/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-console.log('hello');
-var darkModePreference = null;
-var getUserDarkModePreference = function getUserDarkModePreference(darkModePreference) {
-  return darkModePreference !== null && darkModePreference !== void 0 ? darkModePreference : true;
-};
